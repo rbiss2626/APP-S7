cf5e240eace0ffe76b32b074169ac83c41e07f86
rbiss2626 <raphael.bisson@usherbrooke.ca>
2024-01-19 15:28:24 -0500

    optimizer working

--- Problematique/dnn_framework/student/optimizers.py
+++ Problematique/dnn_framework/student/optimizers.py
@@ -7,7 +7,8 @@ class SgdOptimizer(Optimizer):
     """
 
     def __init__(self, parameters, learning_rate=0.01):
-        raise NotImplementedError()
+        Optimizer.__init__(self, parameters)
+        self.learning_rate = learning_rate
 
     def _step_parameter(self, parameter, parameter_grad, parameter_name):
-        raise NotImplementedError()
+        return parameter - self.learning_rate*parameter_grad
